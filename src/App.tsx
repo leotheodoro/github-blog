c7ffeac27829edf353a75fff9db9d1e63b15a15e
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-01-23 15:38:57 -0300

    💄 global styled and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
-  return <h1>Github Blog</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Github Blog</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }

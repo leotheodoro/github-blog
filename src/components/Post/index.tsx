6a671d043891820146fbb3147f9e668ce9bbf90e
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-01-25 17:21:06 -0300

    :construction: props on post

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,21 +1,22 @@
 import { Link } from 'react-router-dom'
 import { PostContainer } from './styles'
 
-export function Post() {
+interface PostProps {
+  id: number
+  title: string
+  body: string
+  createdAt: string
+}
+
+export function Post({ id, title, body, createdAt }: PostProps) {
   return (
     <PostContainer>
       <div>
-        <Link to="/po">JavaScript data types and data structures</Link>
-        <span>Há 1 dia</span>
+        <Link to={`/${id}`}>{title}</Link>
+        <span>{createdAt}</span>
       </div>
 
-      <p>
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in JavaScript and what properties
-        they have. These can be used to build other data structures. Wherever
-        possible, comparisons with other languages are drawn.
-      </p>
+      <p>{body}</p>
     </PostContainer>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,16 @@ import { Profile } from '../../components/Profile'
 import { SearchForm } from './components/SearchForm'
 import { PostsContainer, PostsList } from './styles'
 
+const postData = {
+  title: 'JavaScript data types and data structures',
+  created_at: 'Há 1 dia',
+  body: `Programming languages all have built-in data structures, but these often
+  differ from one language to another. This article attempts to list the
+  built-in data structures available in JavaScript and what properties
+  they have. These can be used to build other data structures. Wherever
+  possible, comparisons with other languages are drawn.`,
+}
+
 export function Home() {
   return (
     <>
@@ -13,13 +23,48 @@ export function Home() {
       <PostsContainer>
         <SearchForm />
         <PostsList>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
+          <Post
+            id={Math.ceil(Math.random() * 999)}
+            body={postData.body}
+            createdAt={postData.created_at}
+            title={postData.title}
+          />
         </PostsList>
       </PostsContainer>
     </>
